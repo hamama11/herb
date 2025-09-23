331413947df40852308f586eac3d3dcf39eed294
hamama11 <ydi74@sonline20.sen.go.kr>
2025-09-23 16:02:51 +0900

    Update 04_This is 나 선.py

--- pages/04_This is 나 선.py
+++ pages/04_This is 나 선.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 st.components.v1.html(
     '''
-    <iframe src="https://gamma.app/docs/-0cr7yn6thhxl1ov?mode=doc"></iframe>
+
+ <iframe src="https://gamma.app/docs/-0cr7yn6thhxl1ov?mode=doc="width: 100%; height: 450px" allow="fullscreen" title="대표 SIX 나선"></iframe>
     ''',
     height=620,
     scrolling=True
