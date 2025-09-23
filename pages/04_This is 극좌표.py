1bf4386abc2dbd8af9a70768d82d375445e2e91d
hamama11 <ydi74@sonline20.sen.go.kr>
2025-09-23 14:08:42 +0900

    Update 04_This is 극좌표.py

--- pages/04_This is 극좌표.py
+++ pages/04_This is 극좌표.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+st.components.v1.html(
+    '''
+    <iframe src="https://gamma.app/embed/rm93extpoygc6dn" style="width: 700px; max-width: 100%; height: 450px" allow="fullscreen" title="극좌표 곡선의 길이와 넓이 공식 원리"></iframe>
+    ''',
+    height=620,
+    scrolling=True
+)
