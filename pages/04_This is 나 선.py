1e84924f78e852657cb4d56f6d547225390e6043
hamama11 <ydi74@sonline20.sen.go.kr>
2025-09-23 16:00:52 +0900

    Update 04_This is 나 선.py

--- pages/04_This is 나 선.py
+++ pages/04_This is 나 선.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.components.v1.html(
     '''
-    <iframe src="https://docs.google.com/document/d/1b9maCsoLhBu4swgufvf3QH-GptNbFN6KWa1Ojw_S_kg/edit?usp=sharing"></iframe>
+    <iframe src="https://gamma.app/docs/-0cr7yn6thhxl1ov?mode=doc"></iframe>
     ''',
     height=620,
     scrolling=True
