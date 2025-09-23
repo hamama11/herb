10ec98889d5a94f4bf3782c28eed238f56f823a2
hamama11 <ydi74@sonline20.sen.go.kr>
2025-09-23 16:04:14 +0900

    Update 04_This is 나 선.py

--- pages/04_This is 나 선.py
+++ pages/04_This is 나 선.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
+
 st.components.v1.html(
     '''
-
- <iframe src="https://gamma.app/docs/-0cr7yn6thhxl1ov?mode=doc="width: 100%; height: 450px" allow="fullscreen" title="대표 SIX 나선"></iframe>
+    <iframe src="https://spiral-curves-bnj6aff.gamma.site/"width: 100%; height: 450px" allow="fullscreen" title="극좌표 곡선의 길이와 넓이 공식 원리"></iframe>
     ''',
     height=620,
     scrolling=True
