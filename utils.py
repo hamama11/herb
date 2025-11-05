38a41077005837dbd71ddf3bae8eddd5fdc44819
hamama11 <ydi74@sonline20.sen.go.kr>
2025-11-05 10:37:34 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,11 +7,15 @@ def f1d(x):
 def grad1d(x):
     return 4*x**3 - 9*x**2
 
+def hess1d(x):
+    return 12*x**2 - 18*x
+
 # 2D 함수
 def f2d(X, Y):
     return (X**4 - 3*X**3) + (Y**4 - 3*Y**3) + 2
 
 def grad2d(X, Y):
-    dfdx = 4*X**3 - 9*X**2
-    dfdy = 4*Y**3 - 9*Y**2
-    return dfdx, dfdy
+    return 4*X**3 - 9*X**2, 4*Y**3 - 9*Y**2
+
+def hess2d(X, Y):
+    return 12*X**2 - 18*X, 12*Y**2 - 18*Y
